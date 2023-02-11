c4e784eb33d491382498185c58f00999c99e6ca3
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-12 02:55:58 +0530

    feat: add update_config for toml config

--- crates/loa/src/main.rs
+++ crates/loa/src/main.rs
@@ -1,4 +1,6 @@
-use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, env, fs, time::Instant};
+use toml;
 
 struct Task {
     name: String,
@@ -47,6 +49,7 @@ impl App {
 }
 
 fn main() {
+    config_manager().unwrap();
     try_main().unwrap();
 }
 
@@ -68,6 +71,70 @@ fn try_main() -> anyhow::Result<()> {
     app.remove_task("Do laundry");
 
     app.display_tasks();
+
+    Ok(())
+}
+
+//------------------------------------------------------------------------------
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    general: General,
+    #[serde(rename = "Environment")]
+    environment: Environment,
+}
+
+impl Config {
+    /// Update the values in the Config struct.
+    fn update_config(&mut self, other: &Config) {
+        self.general.path_to_polars_csv = String::from(other.general.path_to_polars_csv.to_owned());
+        self.environment.name = String::from(other.environment.name.to_owned());
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct General {
+    path_to_polars_csv: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Environment {
+    name: String,
+}
+
+const CONFIG_PATH: &str = "crates/loa/src/config.toml";
+/// With `config_manager` easily access and modify the frequently used features of your application through the config.toml file.
+fn config_manager() -> anyhow::Result<()> {
+    {
+        // With this approach, you can access the individual values by using the dot notation (e.g., config.General.path_to_polars_csv).
+        let mut config: Config = toml::from_str(include_str!("config.toml")).unwrap();
+        let path_to_polars_csv = &config.general.path_to_polars_csv.clone();
+        let env_name = &config.environment.name.clone();
+        println!("Path to polars csv: {}", path_to_polars_csv);
+        println!("Environment name: {}", env_name);
+
+        config.update_config(&Config {
+            general: General {
+                path_to_polars_csv: format!("./{now:?}polars.csv", now = Instant::now()),
+            },
+            environment: Environment {
+                name: "development".to_string(),
+            },
+        });
+
+        let curr_dir = env::current_dir().unwrap().join(CONFIG_PATH);
+        dbg!(&curr_dir);
+        // Serialize the `Config` struct to a TOML string.
+        let config = toml::to_string(&config).unwrap();
+
+        fs::write(curr_dir, config).unwrap();
+    }
+    let config: toml::Value = toml::from_str(include_str!("config.toml")).unwrap();
+    let path_to_polars_csv = config["general"]["path_to_polars_csv"].as_str().unwrap();
+    let env_name = config["Environment"]["name"].as_str().unwrap();
+
+    println!("Path to polars csv: {}", path_to_polars_csv);
+    println!("Environment name: {}", env_name);
 
     Ok(())
 }
