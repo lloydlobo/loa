336a5b9a6cb3677ab77cbfcb63cf5df946b5d844
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-12 02:19:22 +0530

    feat: add display_tasks method

--- crates/loa/src/main.rs
+++ crates/loa/src/main.rs
@@ -31,15 +31,43 @@ impl App {
     fn display_tasks(&self) {
         let mut task_names: Vec<&String> = self.tasks.keys().collect();
         task_names.sort_by(|a, b| self.tasks[*b].priority.cmp(&self.tasks[*a].priority));
-        println!("Tasks (by priority): ");
+        println!(
+            r#"
+|-------------------------------
+|  id  |  Tasks (by priority): 
+|-------------------------------"#
+        );
 
-        for name in task_names {
-            let task = &self.tasks[name];
-            println!("{}: {}", task.name, task.priority);
+        for (i, name) in task_names.iter().enumerate() {
+            let task = &self.tasks[*name];
+            println!("|  {i:#2?}  | {}: {}", task.name, task.priority);
         }
+        println!(r#"|-------------------------------"#);
     }
 }
 
 fn main() {
-    println!("loa main");
+    try_main().unwrap();
+}
+
+fn try_main() -> anyhow::Result<()> {
+    let mut app = App::new();
+
+    HashMap::from([
+        ("Take out the trash", 1),
+        ("Do laundry", 3),
+        ("Clean kitchen", 2),
+    ])
+    .into_iter()
+    .for_each(|(name, priority)| {
+        app.add_task(name, priority);
+    });
+
+    app.display_tasks();
+
+    app.remove_task("Do laundry");
+
+    app.display_tasks();
+
+    Ok(())
 }
