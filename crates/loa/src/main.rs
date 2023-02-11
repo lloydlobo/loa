92a6a61c8512fe0308d26d721b61710b554a31c4
Lloyd Lobo <lloydlobo@gmail.com>
2023-02-12 01:56:32 +0530

    feat: add data structure for app

--- crates/loa/src/main.rs
+++ crates/loa/src/main.rs
@@ -1,3 +1,22 @@
+use std::collections::HashMap;
+
+struct Task {
+    name: String,
+    priority: i32,
+}
+
+struct App {
+    tasks: HashMap<String, Task>,
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            tasks: HashMap::new(),
+        }
+    }
+}
+
 fn main() {
     println!("loa main");
 }
